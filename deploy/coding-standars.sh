104cf67764b516e7391d6ff7322d446254e60c90
Dragos Cirjan <dragos.cirjan@itmediaconnect.ro>
2015-11-11 12:31:05 +0200

    coding standards updates

---
+++ deploy/coding-standars.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+export CHROME_WRAPPER="`readlink -f "$0"`"
+HERE="`dirname "$CHROME_WRAPPER"`"
+
+which npm > /dev/null && {
+    echo 'No node/npm installed.'
+}
+
+which grunt > /dev/null && {
+    echo 'No grunt installed.'
+}
+
+git clone https://github.com/dragoscirjan/coding-standards.git /tmp/coding-standards
+cd /tmp/coding-standards && npm install && grunt && cp -rdf docs/* $HERE/../coding-standards
+
+cd $HERE/..
+git add . -f && git commit -a -m "coding standards updates" && git push origin master
